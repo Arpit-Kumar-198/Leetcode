cee513007c5965d9af55490a571916587b7fb951
Arpit Kumar <arpitkumar19842@gmail.com>
2025-09-02 22:59:27 +0530

    LeetCode 509 Fibonacci Number (DP)

--- leetcode509.cpp
+++ leetcode509.cpp
@@ -6,7 +6,16 @@ int helper(int n, vector<int>& dp){
     return dp[n] = helper(n-1,dp) + helper(n-2,dp);
 }
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return helper(n,dp);
-    }
+        /* Recursion Memoization */
+
+        // vector<int> dp(n+1,-1);
+        // return helper(n,dp);
+
+        /* Tabulation DP */
+        int dp[n+1];
+        dp[0] = 0;
+        if(n >= 1) dp[1] = 1;
+        for(int i = 2; i <= n; i++) dp[i] = dp[i-1] + dp[i-2];
+        return dp[n];
+    } 
 };
