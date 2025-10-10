58a0f0a380b428183ecffaea7cd3b72bfa29d3c7
Arpit Kumar <arpitkumar19842@gmail.com>
2025-10-10 21:36:03 +0530

    Solved leetcode3 - Longest Substring Without Repeating Characters

--- leetcode3.cpp
+++ leetcode3.cpp
@@ -1,50 +1,22 @@
 class Solution {
 public:
-bool ispresent(char ch, string s){
-    for(int i = 0; i < s.size(); i++){
-        if(s[i] == ch) return true;
-    }
-    return false;
-}
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        // if(n == 0) return 0;
-        
-        // int mxlen = INT_MIN;
-        // for(int i = 0; i < n; i++){
-        //     int j = i;
-        //     string t = "";
-        //     while(j < n && ispresent(s[j],t) == false){
-        //         t += s[j];
-        //         mxlen = max(mxlen,(int)t.size());
-        //         j++;
-        //     }
-
-        // }
-        // return mxlen;
-
-        if(n == 0) return 0;
-        unordered_map<char,bool> mp;
-        for(int i = 0; i < n; i++){
-            mp[s[i]] = false;
-        }
-        int mxlen = INT_MIN;
+        int ans = 0;
         int i = 0, j = 0;
-
+        unordered_set<char> st;
         while(j < n){
-            if(mp.find(s[j]) != mp.end()){
-                if(mp[s[j]] == true){
-                    mxlen = max(mxlen,j-i);
-                    mp[s[i]] = false;
-                    i++;
-                }
-                else{
-                    mp[s[j]] = true;
-                    j++;
-                }
+            if(st.find(s[j]) == st.end()){
+                st.insert(s[j]);
+                j++;
+            }
+            else{
+                ans = max(ans,j-i);
+                st.erase(s[i]);
+                i++;
             }
         }
-        mxlen = max(mxlen,j-i);
-        return mxlen;
+        ans = max(ans,j-i);
+        return ans;
     }
 };
