e7613c05cc5c4ee833d1ce4c62ed2dd09b82971d
Arpit Kumar <arpitkumar19842@gmail.com>
2025-10-14 20:31:00 +0530

    Solved leetcode239 - Sliding Window Maximum

--- leetcode239.cpp
+++ leetcode239.cpp
@@ -1,29 +1,16 @@
 class Solution {
-    public:
-        vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-            int n = nums.size();
-            vector<int> nge(n);
-            vector<int> ans;
-            nge[n-1] = n;
-            stack<int> st;
-            st.push(n-1);
-            for(int i = n-2; i >= 0; i--){
-                while(st.size()!=0 && nums[st.top()]<=nums[i]) st.pop();
-                if(st.size()==0) nge[i] = n;
-                else nge[i] = st.top();
-                st.push(i);
-            }
-            int i = 0, j = k-1;
-            while(j < n){
-                int l = nums[i];
-                int r = i+k;
-                int k = i;
-                while(nge[k]<r){
-                    k = nge[k];
-                }
-                ans.push_back(nums[k]);
-                i++,j++;
-            }
-            return ans;
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans;
+
+        deque<int> dq;
+        for(int i = 0; i < n; i++){
+            if(!dq.empty() && dq.front() <= i-k) dq.pop_front();
+            while(!dq.empty() && nums[dq.back()] <= nums[i]) dq.pop_back();
+            dq.push_back(i);
+            if(i >= k-1)  ans.push_back(nums[dq.front()]);
         }
-    };
+        return ans;
+    }
+};
