a10f014a69e5ee26b9c81da391c66708ff7a723f
Arpit Kumar <arpitkumar19842@gmail.com>
2025-11-14 21:28:20 +0530

    Solved leetcode972 - Number of Matching Subsequences

---
+++ leetcode972.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int n = words.size();
+        
+        unordered_map<char,vector<int>> mp;
+        for(int i = 0; i < s.size(); i++){
+            mp[s[i]].push_back(i);
+        }
+        int ans = 0;
+        for(auto w : words){
+            int prev = -1;
+            for(int i = 0; i < w.size(); i++){
+                int idx = upper_bound(mp[w[i]].begin(),mp[w[i]].end(),prev) - mp[w[i]].begin();
+                if(idx == mp[w[i]].size()) break;
+                prev = mp[w[i]][idx];
+                if(i == w.size()-1) ans++;
+            }
+        }
+        return ans;
+    }
+};
