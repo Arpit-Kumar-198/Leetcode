2f32de1e5a1cad9567dc219d784ec751585277ac
Arpit Kumar <arpitkumar19842@gmail.com>
2025-12-07 10:10:33 +0530

    Solved leetcode6 - Zigzag Conversion

--- leetcode6.cpp
+++ leetcode6.cpp
@@ -2,37 +2,33 @@ class Solution {
 public:
     string convert(string s, int numRows) {
         if(numRows == 1) return s;
-        int n = s.size();
-        
         string res = "";
-
-        int y = ( numRows - 2 )*2;
-
-        for(int i = 0; i < numRows; i++){
-            if(i == 0 || i == numRows-1) {
-                int x = numRows + (numRows - 2);
-                int j = i;
-                while(j < n){
-                    res += s[j];
-                    j += x;
-                }
-            }
-            else{
-                int x = numRows + (numRows - 2);
-                int j = i;
-                int t = j;
-                while(j < n){
-                    res += s[j];
-                    j += y;
-                    if(j < n) res += s[j];
-                    j = t + x;
-                    t = j;
-                }
-                y -= 2;
-            }
+        int fl = 2*numRows - 2;
+        int m1 = 2*numRows - 4, m2 = 2;
+        for(int i = 0; i < s.size(); ) {
+            res += s[i];
+            i += fl;
         }
-
+        for(int i = 1; i < numRows-1; i++){
+            int j = i;
+            int turn = 0;
+            while(j < s.size()){
+                res += s[j];
+                if(turn == 0) {
+                    j += m1;
+                    turn = 1;
+                }
+                else {
+                    j += m2;
+                    turn = 0;
+                }
+            }
+            m1 -= 2, m2 += 2;
+        }
+        for(int i = numRows-1; i < s.size(); ) {
+            res += s[i];
+            i += fl;
+        }
         return res;
-        
     }
 };
