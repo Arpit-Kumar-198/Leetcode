f46f48b3c64283916ef3f9edac175ed4981967f9
Arpit Kumar <arpitkumar19842@gmail.com>
2025-12-18 11:42:33 +0530

    Solved leetcode338 - Counting Bits

--- leetcode338.cpp
+++ leetcode338.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-int setBit(int n){
-    int t = 0;
-    while(n > 0){
-        if(n%2 != 0) t++;
-        n /= 2;
-    }
-    return t;
-}
     vector<int> countBits(int n) {
         vector<int> res(n+1);
-        for(int i = 0; i <= n; i++){
-            res[i] = setBit(i);
+        res[0] = 0;
+        for(int i = 1; i <= n; i++){
+            int d = i/2;
+            if(i%2 != 0) res[i] = res[d]+1;
+            else res[i] = res[d];
         }
         return res;
     }
