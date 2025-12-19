77a3f82a96e874af75bc17d5d7040cc9603e7a26
Arpit Kumar <arpitkumar19842@gmail.com>
2025-12-19 23:17:25 +0530

    Solved leetcode190 - Reverse Bits

--- leetcode190.cpp
+++ leetcode190.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-int decimal(string s){
-    int res = 0;
-    int d = 0;
-    for(int i = s.size()-1; i >= 0; i--){
-        if(s[i] == '1') res += pow(2,d);
-        d++;
-    }
-    return res;
-}
     int reverseBits(int n) {
-        string s = "";
-        while(n > 0){
-            if(n%2 == 0) s += "0";
-            else s += "1";
-            n /= 2;
+        int x = 31, res = 0, lsb = 1;
+        while(x){
+            int temp = lsb & n;
+            temp = temp << x;
+            res = res | temp;
+            x--;
+            lsb = 1;
+            n = n >> 1;
         }
-        if(s.size() != 32){
-            while(s.size() != 32){
-                s += "0";
-            }
-        }
-        
-        return decimal(s);
+        return res;
     }
-};leetcode201
+};
