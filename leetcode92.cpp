6d0571f4c970392b10c4d54fb006d2a25a2dd4a1
Arpit Kumar <arpitkumar19842@gmail.com>
2025-12-22 12:31:31 +0530

    Solved leetcode92 - Reverse Linked List II

--- leetcode92.cpp
+++ leetcode92.cpp
@@ -9,55 +9,86 @@
  * };
  */
 class Solution {
-    ListNode* reverseList(ListNode* head) {
-        if(head == NULL) return NULL;
-        ListNode* temp = head;
-        head = head->next;
-        temp->next = NULL;
-        while(head!=NULL){
-            ListNode* t = head;
-            head = head->next;
-            t->next = temp;
-            temp = t;
-            
-        }
-        return temp;
-
-    }
 public:
+ListNode* reverse(ListNode* head){
+    if(head== NULL || head->next == NULL) return head;
+    ListNode* temp = head;
+    ListNode* store1 = temp->next;
+    ListNode* store2 = NULL;
+    while(store1){
+        store1 = temp->next;
+        temp->next = store2;
+        store2 = temp;
+        temp = store1;
+    }
+    return store2;
+}
+int length(ListNode* head){
+    ListNode* temp = head;
+    int l = 0;
+    while(temp){
+        l++;
+        temp = temp->next;
+    }
+    return l;
+}
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(left==right) return head;
-        ListNode* temp = head;
-        int n = 1;
+        if(left == right || head->next == NULL) return head;
+        int len = length(head);
+        if(left == 1 && right == len) return reverse(head);
 
-        ListNode* a = NULL;
-        ListNode* b = NULL;
-        ListNode* c = NULL;
-        ListNode* d = NULL;
-        while(temp!=NULL){
-            if(n==left-1){
-                a = temp;
+        ListNode* leftlist = head;
+        ListNode* middlelist = NULL;
+        ListNode* rightlist = NULL;
+        ListNode* temp = leftlist;
+
+        int idx = 1;
+        if(left == 1){
+            while(temp && idx < right){
+                idx++;
+                temp = temp->next;
             }
-            if(n==left){
-                b = temp;
+            rightlist = temp->next;
+            temp->next = NULL;
+            leftlist = reverse(leftlist);
+            temp = leftlist;
+            while(temp->next != NULL) temp = temp->next;
+            temp->next = rightlist;
+            return leftlist;
+        }
+        else if(right == len){
+            while(temp && idx < left-1){
+                idx++;
+                temp = temp->next;
             }
-            if(n==right){
-                c = temp;
-            }
-            if(n==right+1){
-                d = temp;
-            }
-            n++;
+            rightlist = temp->next;
+            temp->next = NULL;
+            rightlist = reverse(rightlist);
+            temp->next = rightlist;
+            return leftlist;
+        }
+        
+        while(temp && idx < left-1){
+            idx++;
             temp = temp->next;
         }
-
-       
-        if(a) a->next = NULL;
-        c->next = NULL;
-        c = reverseList(b);
-        if(a) a->next = c;
-        b->next = d;
-        if(a) return head;
-        return c;
+        idx++;
+        middlelist = temp->next;
+        temp->next = NULL;
+        temp = middlelist;
+        while(temp && idx < right) {
+            idx++;
+            temp = temp->next;
+        }
+        rightlist = temp->next;
+        temp->next = NULL;
+        middlelist = reverse(middlelist);
+        temp = leftlist;
+        while(temp->next != NULL) temp = temp->next;
+        temp->next = middlelist;
+        while(temp->next != NULL) temp = temp->next;
+        temp->next = rightlist;
+        return leftlist;
+        
     }
 };
