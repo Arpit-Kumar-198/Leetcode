382ea33783b70f9dfb18cd4ddb63d129057a6e38
Arpit Kumar <arpitkumar19842@gmail.com>
2025-12-22 11:18:25 +0530

    Solved leetcode1512 - Number of Good Pairs

--- leetcode1512.cpp
+++ leetcode1512.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int n = nums.size();
-        long long cnt = 0;
         unordered_map<int,int> mp;
-        for(int i = 0; i < n; i++) mp[nums[i]]++;
+        for(auto it : nums) mp[it]++;
+        int cnt = 0;
         for(auto it : mp){
-            long long x = it.second;
-            cnt += ((x*(x-1))/2);
+            cnt += (it.second*(it.second-1))/2;
         }
-        return (int)cnt;
+        return cnt;
     }
 };
