a608f9e79d8c0877e291069c21630f1fb4da8dc6
Arpit Kumar <arpitkumar19842@gmail.com>
2026-02-02 19:53:23 +0530

    Solved leetcode128 - Longest Consecutive Sequence

--- leetcode128.cpp
+++ leetcode128.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int n = nums.size();
-        unordered_set<int> s;
-        for(int i = 0; i < n; i++){
-            s.insert(nums[i]);
-        }
-        int longest = 0;
-        for(auto it : s){
-            if (s.find(it - 1) == s.end()) {
-            //find consecutive numbers:
-            int cnt = 1;
-            int x = it;
-            while (s.find(x + 1) != s.end()) {
-                x = x + 1;
-                cnt = cnt + 1;
+        // if(nums.size() == 0) return 0;
+        // sort(nums.begin(), nums.end());
+        // int mxlen = INT_MIN;
+        // int len = 1;
+        // for(int i = 1; i < nums.size(); i++){
+        //     if(nums[i] == nums[i-1]) continue;
+        //     else if(nums[i] == nums[i-1]+1) len++;
+        //     else{
+        //         mxlen = max(mxlen,len);
+        //         len = 1;
+        //     }
+        // }
+        // mxlen = max(mxlen,len);
+        // len = 1;
+        // return mxlen;
+        unordered_set<int> st;
+        for(auto num : nums) st.insert(num);
+        int mxlen = 0;
+        for(auto it : st){
+            if(st.count(it-1) == 0){
+                int len = 1;
+                int next = it+1;
+                while(st.find(next) != st.end()) len++, next++;
+                mxlen = max(len,mxlen);
             }
-            longest = max(longest, cnt);
-            }
         }
-       
-        return longest;
+        return mxlen;
     }
 };
