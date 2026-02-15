630428c3ac75aac594ed4a1b72e7e90caa677b8d
Arpit Kumar <arpitkumar19842@gmail.com>
2026-02-15 11:15:44 +0530

    Solved leetcode1525 - Number of Good Ways to Split a String

--- leetcode1525.cpp
+++ leetcode1525.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int numSplits(string s) {
-        unordered_map<char,int> mp1;
-        unordered_map<char,int> mp2;
-
-        for(auto it : s) mp1[it]++;
-
-        string left = "", right = "";
-        int cnt = 0;
-
-        for(int i = 0; i < s.size(); i++){
-            mp2[s[i]]++;
-            mp1[s[i]]--;
-            if(mp1[s[i]] == 0) mp1.erase(s[i]);
-            if(mp1.size() == mp2.size()) cnt++;
+        int n = s.size();
+        unordered_map<char,int> mp1, mp2;
+        for(auto ch : s) mp2[ch]++;
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            if(mp1.size() == mp2.size()) ans++;
+            mp2[s[i]]--;
+            mp1[s[i]]++;
+            if(mp2[s[i]] == 0) mp2.erase(s[i]);
         }
-        return cnt;
+        return ans;
     }
 };
