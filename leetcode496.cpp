14dca2a59185b9cabee315b9f2e44d0475001f7b
Arpit Kumar <arpitkumar19842@gmail.com>
2026-02-18 14:07:16 +0530

    Solved leetcode496 - Next Greater Element I

---
+++ leetcode496.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans(nums1.size(),-1);
+        stack<int> st;
+        unordered_map<int,int> mp;
+        for(int i = nums2.size()-1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums2[i]) st.pop();
+            if(!st.empty()) mp[nums2[i]] = st.top();
+            st.push(nums2[i]);
+        }
+        for(int i = 0; i < nums1.size(); i++) if(mp.count(nums1[i])) ans[i] = mp[nums1[i]];
+        return ans;
+    }
+};
