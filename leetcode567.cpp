0335737bff42c404d2d40a5e966b874d12cc5ff0
Arpit Kumar <arpitkumar19842@gmail.com>
2026-02-18 11:14:43 +0530

    Solved leetcode567 - Permutation in String

--- leetcode567.cpp
+++ leetcode567.cpp
@@ -3,25 +3,22 @@ public:
     bool checkInclusion(string s1, string s2) {
         int n1 = s1.size();
         int n2 = s2.size();
-        unordered_map<char,int> mp1;
-        unordered_map<char,int> mp2;
+        vector<int> mp1(26,0);
+        vector<int> mp2(26,0);
 
         
         if(n1 <= n2){
-            for(int i = 0; i < n1; i++) mp1[s1[i]]++;
-            int i = 0, j = n1-1;
-            for(int i = 0; i < n1; i++) mp2[s2[i]]++;
+            for(int i = 0; i < n1; i++) mp1[s1[i]-97]++;
+            for(int i = 0; i < n1; i++) mp2[s2[i]-97]++;
+            int i = 1, j = n1;
+            if(mp1 == mp2) return true;
             while(j < n2){
+                if(mp2[s2[i-1]-97] != 0) mp2[s2[i-1]-97]--;
+                mp2[s2[j]-97]++;
                 if(mp1 == mp2) return true;
-                mp2[s2[i]]--;
-                if(mp2[s2[i]] == 0) mp2.erase(s2[i]);
                 i++,j++;
-                if(j < n2) mp2[s2[j]]++;
             }
-            return false;
         }
-        
         return false;
-
     }
 };
