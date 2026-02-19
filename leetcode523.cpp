cc040e78064d616b132b2494ce170621dd91e0ab
Arpit Kumar <arpitkumar19842@gmail.com>
2026-02-20 00:47:00 +0530

    Solved leetcode523 - Continuous Subarray Sum

--- leetcode523.cpp
+++ leetcode523.cpp
@@ -2,28 +2,22 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-       
-
-        vector<int> prefix(n);
-        prefix[0] = nums[0];
-        int pre = prefix[0];
-
-        for(int i = 1; i < n; i++){
-            prefix[i] = nums[i]+pre;
-            pre = prefix[i];
-        }
-        unordered_map<int,pair<int,int>> mp; 
-        
+        for(int i = 1; i < n; i++) nums[i] = nums[i]+nums[i-1];
+        unordered_map<int,pair<int,int>> mp;
+        for(int i = 0; i < n; i++) nums[i] = nums[i]%k;
         for(int i = 0; i < n; i++){
-            if(prefix[i] % k == 0 && i > 0) return true;
-            if(mp.find(prefix[i]%k) != mp.end()){
-                if(i - mp[prefix[i]%k].second > 1)
-                return true;
+            if(nums[i] == 0 && i > 0) return true;
+            if(mp.find(nums[i]) != mp.end()){
+                mp[nums[i]].first++;
+                if(mp[nums[i]].first > 2) return true;
+                if(mp[nums[i]].first == 2 && i-mp[nums[i]].second > 1) 
+                    return true;
             }
-            mp[prefix[i]%k].first++;
-            if(i > 0 && prefix[i] != prefix[i-1]) mp[prefix[i]%k].second = i;
+            else{
+                mp[nums[i]].first++;
+                mp[nums[i]].second = i;
+            }
         }
         return false;
-        
     }
-};
+}; 
