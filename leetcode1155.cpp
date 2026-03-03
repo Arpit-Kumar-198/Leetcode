56259ac565112772d07d3670b29cdfb4cda40e45
Arpit Kumar <arpitkumar19842@gmail.com>
2026-03-04 00:12:03 +0530

    Solved leetcode1155 - Number of Dice Rolls With Target Sum

--- leetcode1155.cpp
+++ leetcode1155.cpp
@@ -3,14 +3,13 @@ public:
 const int mod = 1000000000+7;
 int helper(int n, int k, int tgt, vector< vector<int> >& dp){
     if(n == 0 && tgt == 0) return 1;
-    if(n == 0 && tgt > 0 || tgt < 0) return 0;
-    int sum = 0;
+    if(n == 0 && tgt > 0) return 0;
     if(dp[n][tgt] != -1) return dp[n][tgt];
+    int sum = 0;
     for(int i = 1; i <= k; i++){
-        int x = helper(n-1,k,tgt-i,dp);
-        sum = (sum%mod + x%mod)%mod;
+        if(tgt-i >= 0) sum = (sum%mod + helper(n-1,k,tgt-i,dp)%mod)%mod;
     }
-    return dp[n][tgt] = sum%mod;
+    return dp[n][tgt] = sum;
     
 }
     int numRollsToTarget(int n, int k, int target) {
