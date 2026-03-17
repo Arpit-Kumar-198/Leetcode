e3284921d1154024be72969f3717a18ec7eccc9c
Arpit Kumar <arpitkumar19842@gmail.com>
2026-03-17 23:52:00 +0530

    Solved leetcode543 - Diameter of Binary Tree

--- leetcode543.cpp
+++ leetcode543.cpp
@@ -10,25 +10,42 @@
  * };
  */
 class Solution {
-    public:
-    int maxDia = 0;
-    int height(TreeNode* root){
-            if(root==NULL) return 0;
-            int l = height(root->left);
-            int r = height(root->right);
-            return 1+max(l,r);
-        }
-    void helper(TreeNode* root, int &maxDia){
-         if(root==NULL) return;
-           int dia = height(root->left)+height(root->right);
-           maxDia = max(maxDia,dia);
-           helper(root->left,maxDia);
-          helper(root->right,maxDia);
+public:
+/*    int height(TreeNode* root){
+        if(root == NULL || (!root->left && !root->right)) return 0;
+        
+        int ans = 0;
+        if(root->left) ans = max(ans,1+height(root->left));
+        if(root->right) ans = max(ans,1+height(root->right));
+        return ans;
     }
-        int diameterOfBinaryTree(TreeNode* root) {
-            int maxDia = 0;
-            helper(root,maxDia);
-           return maxDia;
-    
-        }
-    };
+    int helper(TreeNode* root){
+        if(root == NULL || (!root->left && !root->right)) return 0;
+
+        if(root->left && root->right) 
+            return 2+height(root->left)+height(root->right);
+        else if(root->left && !root->right) return 1+height(root->left);
+        else if(!root->left && root->right) return 1+height(root->right);
+        return 0;
+    }
+*/
+    int height(TreeNode* root, int &ans){
+        if(root==NULL) return 0;
+        int leftHeight = height(root->left,ans);
+        int rightHeight = height(root->right,ans);
+        ans = max(ans,leftHeight+rightHeight);
+        return 1+max(leftHeight,rightHeight);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        /*
+            if(root == NULL) return 0;
+            int ans = helper(root);
+            ans = max(ans,diameterOfBinaryTree(root->left));
+            ans = max(ans,diameterOfBinaryTree(root->right));
+            return ans;
+        */
+        int ans = 0;
+        height(root, ans);
+        return ans;
+    }
+};
