e44292fa84aa025dad734f18cd20f653155ea169
Arpit Kumar <arpitkumar19842@gmail.com>
2026-03-17 21:02:16 +0530

    Solved leetcode1358 - Number of Substrings Containing All Three Characters

---
+++ leetcode1358.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size(), i = 0, j = 0, cnt = 0;
+        unordered_map<char,int> mp;
+        while(j < n){
+            mp[s[j]]++;
+            while(i < n && mp.size() == 3){
+                cnt += (n-j);
+                mp[s[i]]--;
+                if(mp[s[i]] == 0) mp.erase(s[i]);
+                i++;
+            }
+            j++;
+        }
+        return cnt;
+    }
+};
